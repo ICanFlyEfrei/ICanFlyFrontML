033965c882293a9f8dc2a086c7e59830505bf9e4
Valentin Labrune <valentin.labrune@gmail.com>
2024-06-05 10:49:37 +0200

    logo and start header

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import Logo from "../assets/IcanFly_Inline.svg"
+
+export const Header = ():JSX.Element => {
+
+    return (
+        <>
+            <img src={Logo} alt="Logo" />
+
+        </>
+    )
+}
---
+++ src/pages/ResearchPage.tsx
@@ -0,0 +1,7 @@
+export const ResearchPage = ():JSX.Element => {
+
+    return (
+        <>
+        </>
+    )
+}
