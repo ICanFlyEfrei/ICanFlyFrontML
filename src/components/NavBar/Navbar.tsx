afe6ea536f2fe9e9ae2cc334d48295da47bba148
jukle <julien.kle02@gmail.com>
2024-07-10 21:44:21 +0200

    Homepage + Link

--- src/components/NavBar/Navbar.tsx
+++ src/components/NavBar/Navbar.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import Logo from "../../assets/IcanFly_Inline.svg"
+import { Link } from "react-router-dom"
 
 export const Navbar: React.FC = () => {
     return (
         <>
             <div className="navbar sm:py-1 flex sm:justify-between justify-around items-center bg-fifth">
-                <div className='text-5xl text-orange_two sm:ml-24 ml-2  text-first font-bold'>ICanFly</div>
+                <Link to="/">
+                    <div className='text-5xl text-orange_two sm:ml-24 ml-2  text-first font-bold select-none'>ICanFly</div>
+                </Link>
                 <div className='flex sm:mr-36 mr:2'>
-                    <button className="btn btn-square hover:bg-third text-white bg-black sm:mr-4 mr-2 sm:w-24 w-20 p-2">Connexion</button>
-                    <button className="btn btn-square hover:bg-third text-white bg-black btn-ghost sm:w-24 w-20 ">Login</button>
+                    <Link to="/login">
+                        <button className="btn btn-square hover:bg-third text-white bg-black sm:mr-4 mr-2 sm:w-24 w-20 p-2">Connexion</button>
+                    </Link>
+                    <Link to="/login">
+                        <button className="btn btn-square hover:bg-third text-white bg-black btn-ghost sm:w-24 w-20 ">Login</button>*
+                    </Link>
                 </div>
             </div>
         </>
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,16 +1,43 @@
+import { Link } from "react-router-dom"
 import Logo from "../../assets/icanfly-high-resolution-logo-transparent.png"
 import './HomePage.css'
 
-export const HomePage = ():JSX.Element => {
+export const HomePage = (): JSX.Element => {
 
 
     return (
-        <>
-            <img className="logo" src={Logo} alt="logo" />
-            <div className="ButtonGroup">
-                <button className="btn ">Connexion</button>
-                <button className="btn">Inscription</button>
+        <>{/* 
+            <div className="text-8xl text-first h-1/4  mt-52 w-3/4 mx-5">
+                Hey wanna catch a flight?
             </div>
+            <div className="text-8xl text-first h-1/4  my-52 ml-0 mx-5 flex flex-row gap-4">
+                <button
+                    className="btn btn-square text-3xl align-middle bg-white text-first flex flex-1 w-full max-w-96 min-h-24">
+                    Register a flight
+                </button>
+                <button
+                    className="btn btn-square text-3xl align-middle bg-white text-first flex flex-1 w-full max-w-96 min-h-24">
+                    Cath a flight
+                </button>
+            </div> */}
+            <div className="grid row grid-cols-2 grid-rows-1 sm:mt-52 mt-20 sm:gap-y-48 gap-y-12 sm:ml-7">
+                <div className="text-8xl text-first row-span-1 col-span-2">
+                    Hey wanna catch a flight?
+                </div>
+                <Link to="/search">
+                    <button
+                        className="btn btn-square sm:text-3xl text-l align-middle bg-white text-first flex flex-1 w-full sm:max-w-96 max-w-36 min-h-24 row-span-1 col-span-1 ml-7">
+                        Register a flight
+                    </button>
+                </Link>
+                <Link to="/find">
+                    <button
+                        className="btn btn-square sm:text-3xl text-lalign-middle bg-white text-first flex flex-1 w-full sm:max-w-96 max-w-36 min-h-24 row-span-1 col-span-1">
+                        Catch a flight
+                    </button>
+                </Link>
+            </div>
+
 
         </>
     )
